17be369b16b268e4fe8625a787336d01c490d0fa
Zero <silverfantacy@gmail.com>
2020-07-03 15:08:20 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/silverfantacy/soduku.git/precache-manifest.a208db8a8c9ffdbffae400f4c2920724.js"
+  "/silverfantacy/soduku/precache-manifest.c5c0da977a0a9ea32745bd3eb24a03ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/silverfantacy/soduku.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/silverfantacy/soduku/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
