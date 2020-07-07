0f843b330f72fb6f2a91717e96c0e71b28a6c0d6
Zero <silverfantacy@gmail.com>
2020-07-07 11:06:54 +0800

    亂數填入(沒有驗證成功) v2

--- src/App.jsx
+++ src/App.jsx
@@ -217,18 +217,94 @@ function App() {
 
   // 創建數獨
   async function create() {
-    let new_arr = JSON.parse(JSON.stringify(soduku))
+    let new_arr = JSON.parse(JSON.stringify(blank_arr))
     // console.log('new_arr', new_arr)
 
     // let randomNub = 1 + (Math.floor(Math.random() * 10));
     // console.log('randomNub', randomNub)
+    new_arr[0] = randomArr()
+    new_arr[4] = randomArr()
+    new_arr[8] = randomArr()
+    console.log('new_arr 1st =>', new_arr)
+    setSuduku(new_arr)
     await new_arr.forEach((i, iIndex) => {
-      // i.forEach((j,jIndx)=>{
+      if (iIndex === 0 || iIndex === 3 || iIndex ===6) {
+        return
+      }
+      // 大規則
 
-      let use_data = JSON.parse(JSON.stringify(basic_data))
+      // 列判斷
+      let used_data = []
+      
+      i.forEach((j,jIndex) => {
+        let this_row_area;
+        switch (iIndex) {
+          case 0:
+          case 1:
+          case 2:
+            this_row_area = [0, 1, 2]
+            break;
+          case 3:
+          case 4:
+          case 5:
+            this_row_area = [3, 4, 5]
+            break;
+          case 6:
+          case 7:
+          case 8:
+            this_row_area = [6, 7, 8]
+            break;
+          default:
+            break;
+        }
+
+        let this_row;
+        switch (jIndex) {
+          case 0:
+          case 1:
+          case 2:
+            this_row = [0, 1, 2]
+            break;
+          case 3:
+          case 4:
+          case 5:
+            this_row = [3, 4, 5]
+            break;
+          case 6:
+          case 7:
+          case 8:
+            this_row = [6, 7, 8]
+            break;
+          default:
+            break;
+        }
+        // console.log('this_row_area', this_row_area)
+        // console.log('this_row', this_row)
+        let new_suduku = new_arr || JSON.parse(JSON.stringify(soduku))
+        let new_row = []
+        // console.log('new_suduku', new_suduku)
+        this_row_area.forEach(x => {
+          this_row.forEach(y => {
+            new_row.push(new_suduku[x][y])
+          })
+        });
+        used_data = new_row
+
+        console.log('目前有的數字',new_row);
+      })
+
+      let use_data = JSON.parse(JSON.stringify(basic_data)).filter(e=> {
+        if(e !== 0) {
+          return used_data.indexOf(e) === -1
+        }
+      })
+
+      // let use_data = JSON.parse(JSON.stringify(basic_data))
       let a = use_data.length
+      console.log('use_data', use_data,a);
       let temp
       let new_suduku = []
+
       while (a > 0) {
         let tmp_data = use_data
         // console.log('while tmp_data', tmp_data)
@@ -240,27 +316,47 @@ function App() {
         // console.log('indeOf =>', tmp_data.indexOf(temp))
         tmp_data = JSON.parse(JSON.stringify(tmp_data.splice(tmp_data.indexOf(temp), 1)))
         // console.log('use_data end =>', tmp_data)
-        new_suduku.push(temp)
+
+        let index = 9 - a
+        new_suduku[index] = (temp)
         a--
       }
-      // console.log('new_suduku',new_suduku)
-      new_arr[iIndex] = new_suduku
+      console.log('new_suduku', new_suduku);
+
+      
+      // 大規則
+      // new_arr[iIndex] = randomArr()
+      console.log('new_arr end =>', new_arr)
+
+      // setSuduku(new_arr)
     })
-    console.log('new_arr end =>', new_arr)
-    await new_arr.forEach((i, area) => {
-      // console.log('i, area', i, area)
-      i.forEach((j, index) => {
-        // console.log('j, index', j, index)
-        if (checkNumError(i, area, j, index, new_arr)) {
-          console.log('驗證錯誤')
-          // 重新產生陣列
-          // create()
-        } else {
-          console.log('通過')
-        }
-      })
-    })
-    setSuduku(new_arr)
+  }
+
+  function randomArr() {
+    let use_data = JSON.parse(JSON.stringify(basic_data))
+    let a = use_data.length
+    let temp
+    let new_suduku = []
+
+    while (a > 0) {
+      let tmp_data = use_data
+      // console.log('while tmp_data', tmp_data)
+      // console.log('while a',a)
+      let randomNub = (Math.floor(Math.random() * tmp_data.length) + 1);
+      // console.log('while randomNub', randomNub)
+      temp = tmp_data[randomNub - 1]
+      // console.log('while temp', temp)
+      // console.log('indeOf =>', tmp_data.indexOf(temp))
+      tmp_data = JSON.parse(JSON.stringify(tmp_data.splice(tmp_data.indexOf(temp), 1)))
+      // console.log('use_data end =>', tmp_data)
+
+      let index = 9 - a
+      new_suduku[index] = (temp)
+      a--
+    }
+    // console.log('new_suduku',new_suduku)
+
+    return new_suduku
   }
 
   function reset() {
@@ -277,10 +373,10 @@ function App() {
         </ul>
 
         <div className={classes.root}>
-          <Button variant="contained" size="small" onClick={() => {create()}}>
+          <Button variant="contained" size="small" onClick={() => { create() }}>
             重新生產
           </Button>
-          <Button variant="contained" color="secondary" size="small" onClick={() => {reset()}}>
+          <Button variant="contained" color="secondary" size="small" onClick={() => { reset() }}>
             清空
           </Button>
         </div>
